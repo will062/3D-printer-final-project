eca25cfd653b28a0c2c9f5fff8fb8ce67cf2a794
will062 <64812612+will062@users.noreply.github.com>
2026-01-14 13:12:18 -0800

    Update stepperMotorCode.ino

--- Code for Testing on Arduino/stepperMotorCode.ino
+++ Code for Testing on Arduino/stepperMotorCode.ino
@@ -3,23 +3,25 @@ const int PUL = 3;   // Step pin
 const int DIR = 4;   // Direction pin
 
 void setup() {
+  //Set the pinout to output for both Pulse and DIR
   pinMode(PUL, OUTPUT);
-  pinMode(DIR, OUTPUT);
+  pinMode(DIR, OUTPUT); 
 
   digitalWrite(DIR, HIGH); // Set direction
 }
 
 void loop() {
-  // One revolution example (assume 200 steps)
+  // One revolution example (assume 400 steps)
+  //Switch Directions
   digitalWrite(DIR, HIGH);
-
+  //send 400 pulses to the motor on each rising edge the motor will set distance.
   for (int i = 0; i < 400; i++) {
     digitalWrite(PUL, HIGH);
     delayMicroseconds(1000);
     digitalWrite(PUL, LOW);
     delayMicroseconds(1000);
   }
-
+  //Switch direction and repeat the previous step
   digitalWrite(DIR, LOW);
 
   for (int i = 0; i < 400; i++) {
@@ -28,5 +30,6 @@ void loop() {
     digitalWrite(PUL, LOW);
     delayMicroseconds(1000);
   }
+  //create a delay to prevent the motor 
   delay(1000);
 }
